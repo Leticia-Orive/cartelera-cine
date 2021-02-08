76a7c6bc47eeb52d7a630673cee3b40204aa2ff7
Leticia-Orive <77327799+Leticia-Orive@users.noreply.github.com>
2021-02-08 19:00:36 +0100

    Add files via upload

---
+++ src/main/java/entitis/Pelicula.java
@@ -0,0 +1,77 @@
+package entitis;
+
+
+	import java.io.Serializable;
+
+	import javax.persistence.Column;
+	import javax.persistence.Entity;
+	import javax.persistence.GeneratedValue;
+	import javax.persistence.GenerationType;
+	import javax.persistence.Id;
+	import javax.persistence.Table;
+
+
+		@Entity
+		@Table(name="pelicula")
+		public class Pelicula implements Serializable {
+			private static final long serialVersionUID = 1L;
+			@Id
+			@GeneratedValue(strategy=GenerationType.IDENTITY)
+			@Column(name="id")
+			 private Long id;
+			@Column(name="nombre")
+			 private String nombre;
+			@Column(name="duracion")
+			private Integer duracion;
+			
+			
+			
+			
+			public Pelicula(){}
+
+
+
+
+			public Long getId() {
+				return id;
+			}
+
+
+
+
+			public void setId(Long id) {
+				this.id = id;
+			}
+
+
+
+
+			public String getNombre() {
+				return nombre;
+			}
+
+
+
+
+			public void setNombre(String nombre) {
+				this.nombre = nombre;
+			}
+
+
+
+
+			public Integer getDuracion() {
+				return duracion;
+			}
+
+
+
+
+			public void setDuracion(Integer duracion) {
+				this.duracion = duracion;
+			}
+			
+			
+
+
+}
